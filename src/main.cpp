25c9cf463cd987d9120604459d9f25ef02829e0b
WNowak <bulion@zoho.com>
2021-04-10 01:13:08 +0200

    clear main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
-#include "RBTree/RBTree.h"
-
 using namespace std;
 
 int main() {
-    RBTree tree(5);
-
-    tree.add(0);
-    tree.add(1);
-    tree.add(2);
-    tree.add(3);
-    tree.add(4);
-
-    tree.remove(2);
-
     return 0;
 }
